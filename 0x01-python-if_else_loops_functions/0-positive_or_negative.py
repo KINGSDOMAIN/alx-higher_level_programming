73300ef324ca695a3e7e8d23d55ce65576333f8a
kingsley <kingsdomain50@gmail.com>
2024-01-26 17:36:18 -0800

    Positive or negative

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import random
+number = random.randint(-10, 10)
 
 number = random.randint(-10, 10)
 if number > 0:
-    print(number, ' is positive\n')
+    print(f'{number}   is positive\n')
 elif number == 0:
-    print(number, ' is zero\n')
+    print(f'{number} is zero\n')
 else:
-    print(number, ' is negative\n')
+    print(f'{number} is negative\n')
+
--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 last_digit = abs(number) %10
 if last_digit < 0:
     last_digit = -(last_digit)
-    string_holder = 'the last digit of {} is {}'.format(number, last digit)
+    string_holder = "the last digit of {} is {}".format(number, last_digit)
 if number > 5:
     print(f"{string_holder} and is greater than 5\n")
 elif number == 0:
