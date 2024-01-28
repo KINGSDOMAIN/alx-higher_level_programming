2a444a1ef651d7a5d880a2ab9fe1e3d7432bbb6d
kingsley <kingsdomain50@gmail.com>
2024-01-27 16:04:25 -0800

    last digit 

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,5 +10,5 @@ if last_digit > 5:
 elif last_digit == 0:
     print(f"{strholder} and is 0")
 elif last_digit < 6:
-    print(f"{strholder} and is less than 6 and not 0\n")
+    print(f"{strholder} and is less than 6 and not 0")
 
